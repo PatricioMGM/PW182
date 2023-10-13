a16f9665bdb0847607e031b936393636bcf9267a
PatricioMGM <115204170+PatricioMGM@users.noreply.github.com>
2023-10-13 16:35:26 -0600

    uso de controller para las rutas

--- practica3/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-
-Route::view('/', 'welcome')->name('apodoInicio');
-Route::view('/formulario', 'formulario')->name('apodoFormulario');
-Route::view('/recuerdos', 'recuerdos')->name('apodoRecuerdos');
---
+++ practica5/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\diarioController;
+
+Route::get('/', [diarioController::class, 'metodoInicio'])->name('apodoInicio');
+Route::get('/formulario', [diarioController::class, 'metodoFormulario'])->name('apodoFormulario');
+Route::get('/recuerdos', [diarioController::class, 'metodoRecuerdo'])->name('apodoRecuerdos');
+
+
+/* Route::view('/', 'welcome')->name('apodoInicio');
+Route::view('/formulario', 'formulario')->name('apodoFormulario');
+Route::view('/recuerdos', 'recuerdos')->name('apodoRecuerdos'); */
+
